5fed66b788f5c081d97d551d730a54fbd882b626
MMTabaka <miloszmtabaka@gmail.com>
2022-02-23 23:08:32 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Milosz Tabaka on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
